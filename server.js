07021fc0c2d1ea72d21be8cc494196108bac6097
Manasa Manchineni <mmanchin@uncc.com>
2023-10-09 06:18:54 +0530

    NodeJS Authentication test

--- server.js
+++ server.js
@@ -40,26 +40,26 @@ let users = [
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
 
-    for (let user of  users) {
+    for (let user of users) {
         if (username == user.username && password == user.password) {
-            let token = jwt.sign({ id: user.id, username: user.username }, secretKey, {expiresIn :'3m'});
-            res.json({
-                success: true,
-                err: null,
-                token
-            });
-            break; 
+            token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '180s' });
+            break;
         }
-
-
-        else {
-            res.status(401).json({
+    }
+    if (token) {
+        res.json({
+            success: true,
+            err: null,
+            token
+        });
+    }
+    else {
+        res.status(401).json({
             success: false,
             token: null,
             err: 'Username or password is incorrect'
-            });
-        }
-        }
+        });
+    }
 });
 
 app.get('/api/dashboard', jwtMW , (req,res) => {
